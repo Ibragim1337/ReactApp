f8931a1011046449eca965c5b1b50ce13eb07887
Алакпаров Ибрагим Эхтият <Alakp_mr11@itstep.edu.az>
2024-01-28 13:02:07 +0400

    3d task

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,44 @@ import { useState } from "react";
  
 const App = () => {
   let [value, setValue] = useState(1);
+  let [step, setStep] = useState(1);
  
- 
-  const minus = () => {
-    setValue(--value);
+  const minusStep = () => {
+    setValue(value - step);
     console.clear();
+
     console.log(value);
   };
 
-  const plus = () => {
-    setValue(++value);
+  const plusStep = () => {
+    setValue(value + step);
     console.clear();
+
     console.log(value);
+  }
+
+  const plus = () => {
+    setStep(++step);
+    console.clear();
+    console.log(step);
+  }
+
+  const minus = () => {
+    if(step < 2) return;
+    setStep(--step);
+    console.clear();
+    console.log(step);
   }
 
   return (
     <>
+       <button onClick={plusStep}>Plus</button>
+       <div>{value.toString()}</div>
+       <button onClick={minusStep}>Minus</button>
+       <br/>
        <button onClick={plus}>Plus</button>
+       <div>{step.toString()}</div>
        <button onClick={minus}>Minus</button>
-      <div>{value.toString()}</div>
     </>
   );
 };
